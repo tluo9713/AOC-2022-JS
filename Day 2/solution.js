0a9c79f85c6e4ef72f8077e5287a8c81970a742a
Thomas <tluo9713@gmail.com>
2022-12-01 00:32:48 -0500

    generated day 2 directory with files

---
+++ Day 2/solution.js
@@ -0,0 +1,29 @@
+import rawInput from "./input.js";
+import { newLineSplitter } from "../Utils/utils.js";
+
+const sampleInput = ``;
+// process raw input
+let input = newLineSplitter(sampleInput);
+// let input = newLineSplitter(rawInput);
+
+
+let part1 = (input) => {
+    let res = 0;
+    for (let i = 0; i < input.length; i++) {
+        let el = input[i];
+    }
+    for (let i = 0; i < PLACEHOLDER; i++) {
+        
+    }
+    return res;
+};
+
+let part1Solution = part1(input);
+console.log(part1Solution);
+
+let part2 = (input) => {
+    return input;
+};
+
+let part2Solution = part2(input);
+// console.log(part2Solution);
