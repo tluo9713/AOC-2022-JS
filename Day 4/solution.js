1e641ad2674d809af5eb539561e74e3128542c6f
Thomas <tluo9713@gmail.com>
2022-12-03 05:20:34 -0500

    preparation for day 4

---
+++ Day 4/solution.js
@@ -0,0 +1,19 @@
+import rawInput from "./input.js";
+import { newLineSplitter } from "../Utils/utils.js";
+
+const sampleInput = ``;
+// process raw input
+let input = newLineSplitter(sampleInput);
+// input = newLineSplitter(rawInput);
+
+let part1 = (input) => {
+    let res = 0;
+    for (let i = 0; i < input.length - 1; i++) {
+        let el = input[i];
+    }
+    for (let i = 0; i < PLACEHOLDER; i++) {}
+    return res;
+};
+
+let part1Solution = part1(input);
+console.log(part1Solution);
--- scripts/templateGenerator.js
+++ scripts/templateGenerator.js
@@ -22,7 +22,7 @@ import { newLineSplitter } from "../Utils/utils.js";
 const sampleInput = \`\`;
 // process raw input
 let input = newLineSplitter(sampleInput);
-// let input = newLineSplitter(rawInput);
+// input = newLineSplitter(rawInput);
 
 
 let part1 = (input) => {
@@ -38,16 +38,9 @@ let part1 = (input) => {
 
 let part1Solution = part1(input);
 console.log(part1Solution);
-
-let part2 = (input) => {
-    return input;
-};
-
-let part2Solution = part2(input);
-// console.log(part2Solution);
 `;
 
-const day = 3;
+const day = 4;
 
 try {
     // generate directory
