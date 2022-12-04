a349fd4d6da50042d404e3cb93e3d6baf6b9e0a6
Thomas <tluo9713@gmail.com>
2022-12-04 00:37:35 -0500

    added a slightly cleaner version

--- Day 4/solution.js
+++ Day 4/solution.js
@@ -55,3 +55,67 @@ console.log(part1Solution);
 
 let part2Solution = part2(input);
 console.log(part2Solution);
+
+// to be honest i think the code i wrote is not too bad already, below is a bit more readable
+
+const convertToRange = (section) => {
+    const arr = section.split("-");
+    return arr.map((el) => Number(el));
+};
+
+const someOverlap = (rangeA, rangeB) => {
+    const [aStart, aEnd] = rangeA;
+    const [bStart, bEnd] = rangeB;
+
+    // if aStart is between bStart and bEnd, there are overlapping
+    if (bStart <= aStart && aStart <= bEnd) return true;
+    // if aEnd is between bStart and bEnd, there are overlapping
+    if (bStart <= aEnd && aEnd <= bEnd) return true;
+    if (aStart <= bStart && bStart <= aEnd) return true;
+    if (aStart <= bEnd && bEnd <= aEnd) return true;
+    return false;
+};
+
+const completeOverlap = (rangeA, rangeB) => {
+    const [aStart, aEnd] = rangeA;
+    const [bStart, bEnd] = rangeB;
+
+    // if aStart is greater than bStart and aEnd is less than bEnd, a is completely within
+    // b
+    if (bStart <= aStart && aEnd <= bEnd) return true;
+    // vice versa
+    if (aStart <= bStart && bEnd <= aEnd) return true;
+    return false;
+};
+
+let part1Clean = (input) => {
+    let res = 0;
+
+    for (let i = 0; i < input.length - 1; i++) {
+        let [sectionA, sectionB] = input[i].split(",");
+        sectionA = convertToRange(sectionA);
+        sectionB = convertToRange(sectionB);
+
+        if (completeOverlap(sectionA, sectionB)) res++;
+    }
+    return res;
+};
+
+let part1SolutionClean = part1Clean(input);
+console.log(part1SolutionClean);
+
+let part2Clean = (input) => {
+    let res = 0;
+
+    for (let i = 0; i < input.length - 1; i++) {
+        let [sectionA, sectionB] = input[i].split(",");
+        sectionA = convertToRange(sectionA);
+        sectionB = convertToRange(sectionB);
+
+        if (someOverlap(sectionA, sectionB)) res++;
+    }
+    return res;
+};
+
+let part2SolutionClean = part2Clean(input);
+console.log(part2SolutionClean);
