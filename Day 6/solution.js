efc8741b9a06eca987addc2af99be894bbe037a1
Thomas <tluo9713@gmail.com>
2022-12-06 00:08:02 -0500

    initial solution

--- Day 6/solution.js
+++ Day 6/solution.js
@@ -1,22 +1,38 @@
 import rawInput from "./input.js";
 import { newLineSplitter } from "../Utils/utils.js";
 
-const sampleInput = ``;
+const sampleInput = `bvwbjplbgvbhsrlpgdmjqwftvncz`;
 // process raw input
-let input = newLineSplitter(sampleInput);
-// input = newLineSplitter(rawInput);
-
+let input = sampleInput;
+input = rawInput;
 
 let part1 = (input) => {
     let res = 0;
-    for (let i = 0; i < input.length - 1; i++) {
-        let el = input[i];
+    for (let i = 3; i < input.length - 1; i++) {
+        let a = input[i - 3];
+        let b = input[i - 2];
+        let c = input[i - 1];
+        let d = input[i];
+        let set = new Set([a, b, c, d]);
+        if (set.size == 4) return i + 1;
     }
-    for (let i = 0; i < PLACEHOLDER; i++) {
-        
+    return res;
+};
+
+let part2 = (input) => {
+    let res = 0;
+    for (let i = 3; i < input.length - 1; i++) {
+        let set = new Set();
+        for (let j = i - 13; j <= i; j++) {
+            set.add(input[j]);
+        }
+        if (set.size == 14) return i + 1;
     }
     return res;
 };
 
 const part1Solution = part1(input);
 console.log(part1Solution);
+
+const part2Solution = part2(input);
+console.log(part2Solution);
