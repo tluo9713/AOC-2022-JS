e95a586897118e01b7fdfda6ad0032cd8babfe99
Thomas <tluo9713@gmail.com>
2022-12-09 04:20:37 -0500

    refactored

--- Day 9/solution.js
+++ Day 9/solution.js
@@ -97,3 +97,78 @@ console.log(part1Solution);
 
 const part2Solution = part2(input);
 console.log(part2Solution);
+
+// just make it cleaner to be honest
+
+const moveTail = (head, tail) => {
+    const [headX, headY] = head;
+    let [tailX, tailY] = tail;
+    // if head is not far enough away from tail, we dont move at all
+    if (Math.abs(headX - tailX) <= 1 && Math.abs(headY - tailY) <= 1)
+        return tail;
+
+    // if tail only needs to move one direction once
+    const tailDoesNotMoveX = headX == tailX;
+    const tailDoesNotMoveY = headY == tailY;
+
+    if (headY == tailY + 2 && tailDoesNotMoveX) return [tailX, tailY + 1];
+    if (headY == tailY - 2 && tailDoesNotMoveX) return [tailX, tailY - 1];
+    if (headX == tailX + 2 && tailDoesNotMoveY) return [tailX + 1, tailY];
+    if (headX == tailX - 2 && tailDoesNotMoveY) return [tailX - 1, tailY];
+
+    // at this point we are dealing with diagonals so we move x and y once so tail is closer to
+    // head
+    tailX += headX < tailX ? -1 : 1;
+    tailY += headY < tailY ? -1 : 1;
+    return [tailX, tailY];
+};
+
+const dir = { R: [0, 1], L: [0, -1], U: [-1, 0], D: [1, 0] };
+const moveWholeRope = (rope) => {
+    for (let i = 0; i < rope.length - 1; i++) {
+        let head = rope[i];
+        let tail = rope[i + 1];
+        tail = moveTail(head, tail);
+        rope[i + 1] = tail;
+    }
+    return rope;
+};
+
+const getTailPositionsVisited = (input, ropeLength) => {
+    const rope = [];
+    for (let i = 0; i < ropeLength; i++) rope.push([0, 0]);
+    const visitedPositions = new Set(["0,0"]);
+    for (let i = 0; i < input.length - 1; i++) {
+        let [direction, count] = input[i].split(" ");
+        let [x, y] = dir[direction];
+        count = Number(count);
+        for (let j = 0; j < count; j++) {
+            // move head
+            const head = rope[0];
+            head[0] += x;
+            head[1] += y;
+            // move rope
+            moveWholeRope(rope);
+            // add to set
+            const tailPosiition = rope.at(-1);
+            visitedPositions.add(tailPosiition.join(","));
+        }
+    }
+    return visitedPositions.size;
+};
+
+const part1Refactor = (input) => {
+    const res = getTailPositionsVisited(input, 2);
+    return res;
+};
+
+const part1RefactorSolution = part1Refactor(input);
+console.log(part1RefactorSolution);
+
+const part2Refactor = (input) => {
+    const res = getTailPositionsVisited(input, 10);
+    return res;
+};
+
+const part2RefactorSolution = part2Refactor(input);
+console.log(part2RefactorSolution);
