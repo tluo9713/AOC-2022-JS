f57766906e4db3babbd192e7cab18f1ec347a1fd
Thomas <tluo9713@gmail.com>
2022-12-10 06:16:58 -0500

    refactored solution

--- Day 10/solution.js
+++ Day 10/solution.js
@@ -1,7 +1,7 @@
 import rawInput from "./input.js";
 import { newLineSplitter } from "../Utils/utils.js";
 
-const sampleInput = `addx 15
+let sampleInput = `addx 15
 addx -11
 addx 6
 addx -3
@@ -154,7 +154,7 @@ let input = newLineSplitter(sampleInput);
 input = newLineSplitter(rawInput);
 
 // again, initial solutions are saved as is, only removing debugging statements. this is to show
-// how ugly it can get and reading is so difficult
+// how ugly it can get and reading the problem is so difficult
 let part1 = (input) => {
     let x = 1;
     let res = 0;
@@ -226,3 +226,66 @@ let part2 = (input) => {
 
 const part2Solution = part2(input);
 for (let i = 0; i < part2Solution.length; i++) console.log(part2Solution[i]);
+
+// i think we should instead have it iterate through cycles and if we need to we read from
+// line and do the input. if noop, just continue on
+// if do something, wait the appropriate cycle, then perform and clear up action to do
+
+// for part 2, im thinking cyclecount >= x && cyclecount <= x+ size where size = size of pixel-1
+
+const processInstructions = (input, callback) => {
+    let x = 1;
+    let dict = { noop: 1, addx: 2 };
+    for (let inputIdx = 0, cycle = 1; cycle <= 240; inputIdx++) {
+        const [instruction, val] = input[inputIdx].split(" ");
+        let cyclesToComplete = dict[instruction];
+
+        while (cyclesToComplete) {
+            callback(x, cycle);
+            cycle++;
+            cyclesToComplete--;
+        }
+        if (instruction == "addx") x += +val;
+    }
+};
+
+let part1Refactor = (input) => {
+    let res = 0;
+    const importantCycles = new Set([20, 60, 100, 140, 180, 220]);
+    // to make this not reliant on a set, we could use (cycle + 20 % 40 == 0) but i find
+    // this is less readable
+
+    const addSignalStrength = (x, cycle) => {
+        // add only if its at the cycles we're interested in
+        if (importantCycles.has(cycle)) res += x * cycle;
+    };
+
+    processInstructions(input, addSignalStrength);
+    return res;
+};
+
+const part1RefactorSolution = part1Refactor(input);
+console.log(part1RefactorSolution);
+
+let part2Refactor = (input) => {
+    let res = [];
+    let curr = "";
+    const sizeOfSprite = 3;
+    const drawCRT = (x, cycle) => {
+        // the size of the CRT is 40, so as cycles exceeds 40, we take the mod to adjust
+        cycle = cycle % 40;
+        const cycleContainsSprite = cycle >= x && cycle <= x + sizeOfSprite - 1;
+        // if cycle contains sprite, we draw with a "#", else "." represents empty
+        curr += cycleContainsSprite ? "#" : ".";
+        if (curr.length !== 40) return;
+        res.push(curr);
+        curr = "";
+    };
+
+    processInstructions(input, drawCRT);
+    return res;
+};
+
+const part2RefactorSolution = part2Refactor(input);
+for (let i = 0; i < part2RefactorSolution.length; i++)
+    console.log(part2RefactorSolution[i]);
